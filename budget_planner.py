e337a4da3f1c79d981c510ac4551952b5d00f21d
Kurt Almoite <kurtalmoite@gmail.com>
2024-03-01 09:46:57 -0500

    Started preparing table for data, generating pdf now accepts parameters.

--- budget_planner.py
+++ budget_planner.py
@@ -5,12 +5,20 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import color 
 
 
-def generate_pdf():
+def generate_pdf(budget, expenses, total_expenses):
     #Create pdf after user enters expense
-    doc = SimpleDocTemplate("Budget_Planner.pdf")
+    doc = SimpleDocTemplate("Budget_Planner.pdf" pagesizes= letter) 
+    store_data = []
+
+    #Prepare table for data
+    data = [["Category", "Amount"]] 
+    data.extend([[category, f"${amount:.2f}"] for category,amount in expense.items()])
+ 
+    #Create table for Budget and Expense
+    
 
 def budget_planner():
-
+    
     #Collect total budget from user
     budget = float(simpledialog.askstring("Input", "Enter the total budget:"))
     expenses = {}
