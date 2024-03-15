677e8899f0d5990fc4fd20b5edfbd622f5892d55
Kurt Almoite <almoitekurt@gmail.com>
2024-03-14 21:01:14 -0400

    Fixed bugs. program works.

--- budget_planner.py
+++ budget_planner.py
@@ -7,52 +7,49 @@ from reportlab.lib import colors
 def generate_pdf(budget, expenses, total_expenses):
     #Create pdf after user enters expense
     doc = SimpleDocTemplate("Budget_Planner.pdf" , pagesizes= letter) 
-    store_data = []
+    story = []
 
     #Prepare table for data
     data = [["Category", "Amount"]] 
     data.extend([[category, f"${amount:.2f}"] for category,amount in expenses.items()])
-    data.append([["Total Expenses", f"${total_expenses:.2f}"]])
-    data.append([["Budget", f"${budget - total_expenses:.2f}"]]) #remaining budget 
+    data.append(["Total Expenses", f"${total_expenses:.2f}"])
+    data.append(["Budget", f"${budget - total_expenses:.2f}"]) #remaining budget 
  
     #Create table for Budget and Expense
-    t = Table(data, colWidths =  [200,150], repeatRows=1)
-    t.setStyleTable(TableStyle([
+    t = Table(data, colWidths=[200,150], repeatRows=1)
+    t.setStyle(TableStyle([
         ('BACKGROUND', (0,0), (-1,0), colors.red),
         ('TEXTCOLOR', (0,0), (-1,0), colors.white),
         ('ALIGN', (0,0), (-1,-1), 'CENTER'),
-<<<<<<< HEAD
-        ('GRID'), (0,0), (-1,-1), 1, colors.black),
-        ('ROWBACKGROUNDS', (0,1), (-1,-1), [colors.lightred, colors.white]),
+        ('GRID', (0,0), (-1,-1), 1, colors.black),
+        ('ROWBACKGROUNDS', (0,1), (-1,-1), [colors.lightblue, colors.white]),
 
     ]))
 
-    
-=======
-
->>>>>>> c0ef127581c6726716a56dcd95f76f6e057fc6ee
-
-    ]))
-
-    store_data.appened(t) #call data to table
-    doc.build(store_data) #build table with stored data that is from input
+    story.append(t) #call data to table
+    doc.build(story) #build table with stored data that is from input
 
 
 def budget_planner():
-    
+    root = tk.Tk()
+    root.withdraw()
+
     #Collect total budget from user
-    budget = float(simpledialog.askstring("Input", "Enter the total budget:"))
+    budget = float(simpledialog.askstring("Input", "Enter the total budget:", parent=root))
     expenses = {}
     total_expenses = 0
 
     #Collect expense from user
     while True:
-        category = simpledialog.askstring("Input", "Enter the expense or 'done' to finish:")
-        if category == 'done':
+        category = simpledialog.askstring("Input", "Enter the expense or 'done' to finish:", parent=root)
+        if category.lower() == 'done':
             break
-    amount = float
+        amount = float(simpledialog.askstring("Input", f"Enter the amount for {category}:", parent=root))
+        expenses[category.capitalize()] = expenses.get(category.capitalize(),0) + amount
+        total_expenses += amount
 
     generate_pdf(budget, expenses,total_expenses)
+    tk.messagebox.showinfo("INFO", "Budget planner PDF!")
 
 if __name__ == '__main__':
     budget_planner()
